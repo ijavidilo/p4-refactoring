876920e180f74c944f8cb26486035f3ecab2f1f4
easy4me <j.diazlop90@gmail.com>
2020-11-27 23:38:03 +0100

    cambiamos la clase principal para que no conozca a la vista sino a los controladores

--- solution.Java/src/main/java/usantatecla/draughts/Draughts.java
+++ solution.Java/src/main/java/usantatecla/draughts/Draughts.java
@@ -1,26 +1,41 @@
 package usantatecla.draughts;
 
-import usantatecla.draughts.controllers.Logic;
 import usantatecla.draughts.controllers.InteractorController;
-import usantatecla.draughts.views.View;
+import usantatecla.draughts.controllers.PlayController;
+import usantatecla.draughts.controllers.ResumeController;
+import usantatecla.draughts.controllers.StartController;
+import usantatecla.draughts.models.Game;
+import usantatecla.draughts.models.State;
+import usantatecla.draughts.models.StateValue;
+
+import java.util.HashMap;
+import java.util.Map;
 
 class Draughts {
-    
-    private View view;
-    private Logic logic;
+
+    private Game game;
+    private State state;
+    private Map<StateValue, InteractorController> controllers;
 
     private Draughts(){
-        this.view = new View();
-        this.logic = new Logic();
+        this.game = new Game();
+        this.state = new State();
+
+        this.controllers = new HashMap<StateValue, InteractorController>();
+        this.controllers.put(StateValue.INITIAL, new StartController(this.game, this.state));
+        this.controllers.put(StateValue.IN_GAME, new PlayController(this.game, this.state));
+        this.controllers.put(StateValue.FINAL, new ResumeController(this.game, this.state));
+        this.controllers.put(StateValue.EXIT, null);
     }
 
     private void play() {
         InteractorController controller;
-		do {
-			controller = this.logic.getController();
-			if (controller != null)
-				this.view.interact(controller);
-		} while (controller != null); 
+        do {
+            controller = this.controllers.get(this.state.getValueState());
+            if (controller != null) {
+                controller.control();
+            }
+        } while (controller != null);
     }
 
     public static void main(String[] args){
