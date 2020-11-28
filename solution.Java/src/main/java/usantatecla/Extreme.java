b945fe889cfa624a4353975f752bb38966bf0a8d
easy4me <j.diazlop90@gmail.com>
2020-11-28 12:32:20 +0100

    Heredamos de la clase Extreme desde Min/Max y movemos los metodos de las clases derivadas a la padre

--- solution.Java/src/main/java/usantatecla/Extreme.java
+++ solution.Java/src/main/java/usantatecla/Extreme.java
@@ -10,4 +10,32 @@ public abstract class Extreme {
 
     public abstract boolean isWithin(double value);
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        long temp;
+        temp = Double.doubleToLongBits(value);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Extreme other = (Extreme) obj;
+        if (Double.doubleToLongBits(value) != Double.doubleToLongBits(other.value))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return this.value + ")";
+    }
 }
