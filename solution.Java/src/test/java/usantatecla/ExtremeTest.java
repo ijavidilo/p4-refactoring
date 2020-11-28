4e8919d2603f61186c93d9b5f51fa5b28673226f
easy4me <j.diazlop90@gmail.com>
2020-11-28 12:00:45 +0100

    Creamos la clase ExtremeTest con el punto

---
+++ solution.Java/src/test/java/usantatecla/ExtremeTest.java
@@ -0,0 +1,14 @@
+package usantatecla;
+
+import org.junit.Before;
+
+public class ExtremeTest {
+
+    protected Point point;
+
+    @Before
+    public void before() {
+        this.point = new Point(4.4);
+    }
+
+}
