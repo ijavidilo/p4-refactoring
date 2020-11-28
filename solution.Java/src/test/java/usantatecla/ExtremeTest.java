6b5775bb925639c9451754350f64eb11473b986e
easy4me <j.diazlop90@gmail.com>
2020-11-28 12:20:05 +0100

    Creamos test sobre el punto en ExtremeTest

--- solution.Java/src/test/java/usantatecla/ExtremeTest.java
+++ solution.Java/src/test/java/usantatecla/ExtremeTest.java
@@ -1,6 +1,10 @@
 package usantatecla;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
 
 public class ExtremeTest {
 
@@ -9,6 +13,19 @@ public class ExtremeTest {
     @Before
     public void before() {
         this.point = new Point(4.4);
+    }
+
+    @Test
+    public void givenPointWhenIsCreatedThenTrue() {
+        assertThat((this.point), is(instanceOf(Point.class)));
+
+        assertThat(this.point.getEquals(), is(this.point.getValue()));
+        assertThat(this.point.getLess(), is(this.point.getValue() - Point.DEVIATION));
+        assertThat(this.point.getGreater(), is(this.point.getValue() + Point.DEVIATION));
+
+        assertThat(this.point.getEquals(), is(not(5555.0)));
+        assertThat(this.point.getLess(), is(not(this.point.getValue() + Point.DEVIATION)));
+        assertThat(this.point.getGreater(), is(not(this.point.getValue() - Point.DEVIATION)));
     }
 
 }
