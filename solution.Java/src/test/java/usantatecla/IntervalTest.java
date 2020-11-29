259c15801168288bf91ff7733f687b38a6396c41
easy4me <j.diazlop90@gmail.com>
2020-11-29 13:14:16 +0100

    FAIL-TEST - Añado test isIntersected

--- solution.Java/src/test/java/usantatecla/IntervalTest.java
+++ solution.Java/src/test/java/usantatecla/IntervalTest.java
@@ -1,10 +1,14 @@
 package usantatecla;
 
+import javafx.util.Pair;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 public class IntervalTest {
 
@@ -79,18 +83,49 @@ public class IntervalTest {
     public void givenOneIntervalTestLeftIsIntersectedTest() {
         Interval interval_one = new IntervalBuilder().open(new Point(-2.2).getEquals()).open(new Point(4.4).getEquals()).build();
 
+        List<Pair<Interval, Boolean>> intervalList = Arrays.asList(
+                new Pair<Interval, Boolean>(new IntervalBuilder().open(new Point(-5.0).getEquals()).open(new Point(-2.2).getEquals()).build(), Boolean.FALSE),
+                new Pair<Interval, Boolean>(new IntervalBuilder().open(new Point(-5.0).getEquals()).closed(new Point(-2.2).getEquals()).build(), Boolean.FALSE),
+                new Pair<Interval, Boolean>(new IntervalBuilder().open(new Point(-5.0).getEquals()).open(new Point(2).getEquals()).build(), Boolean.TRUE),
+                new Pair<Interval, Boolean>(new IntervalBuilder().open(new Point(-5.0).getEquals()).closed(new Point(55).getEquals()).build(), Boolean.TRUE),
+                new Pair<Interval, Boolean>(new IntervalBuilder().open(new Point(-5.0).getEquals()).closed(new Point(-3.0).getEquals()).build(), Boolean.FALSE)
+        );
+
+        for (Pair<Interval, Boolean> interval : intervalList) {
+            assertThat(interval_one.isIntersected(interval.getKey()), is(interval.getValue()));
+        }
     }
 
     @Test
     public void givenOneIntervalTestRightIsIntersectedTest() {
-        Interval interval_one = new IntervalBuilder().open(new Point(-2.2).getEquals()).closed(new Point(4.4).getEquals()).build();
+        Interval interval_one = new IntervalBuilder().open(new Point(-2.2).getEquals()).open(new Point(4.4).getEquals()).build();
 
+        List<Pair<Interval, Boolean>> intervalList = Arrays.asList(
+                new Pair<Interval, Boolean>(new IntervalBuilder().open(new Point(4.4).getEquals()).open(new Point(10).getEquals()).build(), Boolean.FALSE),
+                new Pair<Interval, Boolean>(new IntervalBuilder().closed(new Point(4.4).getEquals()).closed(new Point(10).getEquals()).build(), Boolean.FALSE),
+                new Pair<Interval, Boolean>(new IntervalBuilder().open(new Point(2).getEquals()).open(new Point(10).getEquals()).build(), Boolean.TRUE),
+                new Pair<Interval, Boolean>(new IntervalBuilder().open(new Point(5).getEquals()).closed(new Point(10).getEquals()).build(), Boolean.FALSE)
+        );
+
+        for (Pair<Interval, Boolean> interval : intervalList) {
+            assertThat(interval_one.isIntersected(interval.getKey()), is(interval.getValue()));
+        }
     }
 
     @Test
-    public void givenOneIntervalTestMiddleIsIntersectedTest() {
-        Interval interval_one = new IntervalBuilder().closed(new Point(-2.2).getEquals()).open(new Point(4.4).getEquals()).build();
+    public void givenOneIntervalTestMiddleOpenOpenIsIntersectedTest() {
+        Interval interval_one = new IntervalBuilder().open(new Point(-2.2).getEquals()).open(new Point(4.4).getEquals()).build();
 
+        List<Pair<Interval, Boolean>> intervalList = Arrays.asList(
+                new Pair<Interval, Boolean>(new IntervalBuilder().open(new Point(-2.2).getEquals()).open(new Point(4.4).getEquals()).build(), Boolean.TRUE),
+                new Pair<Interval, Boolean>(new IntervalBuilder().closed(new Point(-2.2).getEquals()).closed(new Point(4.4).getEquals()).build(), Boolean.TRUE),
+                new Pair<Interval, Boolean>(new IntervalBuilder().open(new Point(-2.3).getEquals()).open(new Point(4.5).getEquals()).build(), Boolean.TRUE),
+                new Pair<Interval, Boolean>(new IntervalBuilder().open(new Point(-2.1).getEquals()).open(new Point(4.5).getEquals()).build(), Boolean.TRUE)
+        );
+
+        for (Pair<Interval, Boolean> interval : intervalList) {
+            assertThat(interval_one.isIntersected(interval.getKey()), is(interval.getValue()));
+        }
     }
 
 }
