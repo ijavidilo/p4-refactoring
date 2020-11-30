4fbf04defae748d13f361ca6a791424e09140efe
easy4me <j.diazlop90@gmail.com>
2020-11-30 15:45:27 +0100

    Implementamos el codigo de produccion a la Logic para que trabaje con el Map de estados

--- solution.Java/src/main/java/usantatecla/tictactoe/controllers/Logic.java
+++ solution.Java/src/main/java/usantatecla/tictactoe/controllers/Logic.java
@@ -1,7 +1,25 @@
 package usantatecla.tictactoe.controllers;
 
+import usantatecla.tictactoe.models.Game;
+import usantatecla.tictactoe.types.StateValue;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Logic {
-    public String getController() {
-        return null;
+    private Game game;
+    private Map<StateValue, Controller> controllers;
+
+    public Logic() {
+        this.game = new Game();
+        this.controllers = new HashMap<>();
+        this.controllers.put(StateValue.INITIAL, new StartController(this.game));
+        this.controllers.put(StateValue.IN_GAME, new PlayController(this.game));
+        this.controllers.put(StateValue.RESUME, new ResumeController(this.game));
+        this.controllers.put(StateValue.EXIT, null);
+    }
+
+    public Controller getController() {
+        return this.controllers.get(this.game.getValueState());
     }
 }
